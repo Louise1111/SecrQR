b2af1706289c1cf55312745145dfa97ddb7375f4
Louise1111 <lchristianboysillo@gmail.com>
2024-05-01 13:21:31 +0800

    update settings

--- backendSecQR/settings.py
+++ backendSecQR/settings.py
@@ -156,11 +156,12 @@ USE_I18N = True
 USE_TZ = True
 
 
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -171,9 +172,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-import os
 from django.core.mail.backends.smtp import EmailBackend
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+
 
 # Email backend configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
